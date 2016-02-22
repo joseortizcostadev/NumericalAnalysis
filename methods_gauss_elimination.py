c146ea1d957a88545e00bb7e9cafb1068ea2bb06
Jose  Ortiz <joseortizcosta@icloud.com>
2016-02-21 19:08:18 -0800

    Implemented Script with methods to solve a Matrix Ax=b by the following methods: Gaussus-Naive, Scaled-Partial, LU Factorization and Gauss-Seidel. Also, inside the script is a method to solve a Matrix by Partial-Pivoting but it is not tested in the Method

--- methods_gauss_elimination.py
+++ methods_gauss_elimination.py
@@ -14,13 +14,21 @@
 """
 # Libraries containing the methods supported for this file in order to find
 # solutions for matrices
-import numpy as np
+
 import math
+import numpy as np
 from scipy.integrate import quad
+
+
 
 # Enumerator that enumerates the Gauss methods supported in this file
 class Method:
-    NAIVE, PARTIAL, SCALED_PARTIAL, LU_FACTORIZATION, GAUSS_SEIDER = range(0, 5) 
+    NAIVE = 0
+    PARTIAL = 1
+    SCALED_PARTIAL = 2 
+    LU_FACTORIZATION = 3
+    GAUSS_SEIDEL = 4
+     
 
 # Retursn a exact copy of the matrix given as argument
 def copyMatrix(M):
@@ -57,6 +65,14 @@ def GetRow(mat, row):
     if row >= rows(mat) or row < 0:
        return "Error: row index out of range"
     return(mat[row])
+
+# Adds a column 
+def addColumn (M,col):
+    i = 0
+    for row in M:
+        row.append(col[i][0])
+        i+=1
+    return M
     
 # find the position of the greater pivot
 def pivot_position (mat, rowIndex):
@@ -128,7 +144,32 @@ def scalarMult(a,mat):
         for col in range(cols(mat)):
             mat[row][col] = a*mat[row][col]
     return(mat)
+# Calculates the dot product  
+def Dot(S,T):
+    "return dot product of two vectors"
+    if len(S) != len(T):
+        return "Error: Vectors need to be same size"
+    return sum([S[index] * T[index] for index in range(len(S))])
 
+# Multiply two matrices
+def MultiplyMat(mat1,mat2):
+    "multiply two matrices"
+    if cols(mat1) != rows(mat2):
+        return "Error: number of columns in mat1 must be equal to the number of rows in mat2"
+    return [[Dot(GetRow(mat1,row),GetCol(mat2,col)) for col in range(cols(mat2))] for row in range(rows(mat1))] 
+
+# Add two vectors
+def AddVectors(S,T):
+    "return S+T"
+    if len(S) != len(T):
+       return "Error: Vectors need to be same size"
+    return [S[index] + T[index] for index in range(len(S))]
+    
+#Add two matrices
+def AddMatrices(A,B):
+    "add two matrices"
+    return [AddVectors(A[vectors], B[vectors]) for vectors in range(rows(A))]
+    
 # reduce rows in a matrix   
 def rowReduce(M):
     "returns a row reduced version of M"
@@ -161,15 +202,18 @@ def getRowReduced (mat,rowToReduceIndex,pivotRowIndex):
     
 #   given a row reduced augmented matrix with nonzero 
 #   diagonal entries, returns a solution vector
-def backSub(M):
+def backSub(M, solInMatrixForm):
     "Returns the x's solution of a matrix using back sustitution method"
     cs = cols(M)-1 # cols not counting augmented col
     sol = [0 for i in range(cs)] # place for solution vector
     for i in range(1,cs+1):
         row = cs-i # work backwards
         sol[row] = ((M[row][cs] - sum([M[row][j]*sol[j] for
-                    j in range(row+1,cs)])) / M[row][row]) 
-    return(sol)
+                    j in range(row+1,cs)])) / M[row][row])
+    if (solInMatrixForm):
+        solm = [[sol[nrow] for __ in range(0,1)] for nrow in range (len(sol))]
+        return(solm)
+    return (sol)
     
 # Partial pivoting for Gauss elimination
 def partial_pivoting (mat):
@@ -204,65 +248,189 @@ def LUFactorization (M):
     
 
 # Gauss Seider aproximation
-def Gauss_Seider(M, b, x, n):
-    L = np.tril(M)
+def gauss_seidel(A, b, x=None, n=1000):
+    if x==None:
+        x = [0 for __ in A]
+    b = sum(b, [])
+    L = np.tril(A)
     U = A - L
-    for k in range(n):
+    tmpx= []
+    for i in range(n):
         x = np.dot(np.linalg.inv(L), b - np.dot(U, x))
-    return x
+        if set(tmpx) == set(x) :
+            break;
+        tmpx = x
+    return [[x[row] for __ in range(0,1)] for row in range(0,len(x))]  
+   
 
-# Construct the b column of a Hilbert Matrix   
-def Hilbert_b(x,n):
+# Construct the b column of a Hilbert Matrix  
+# integral of x^n(c)sin(xpi/2)dx
+def Hilbert_b_integral(x,n):
     "Returns the b part of a Hilbert matrix calculated by an integral"
     return math.pow(x,n)*math.sin((math.pi*x)/2)
 
+         
 # Construct the Hilbert matrix for a given N
+# [[1...1/n][....][....][1/n ..... 1/(2n-1)]]
+# This matrix also include the b column
 def Hilbert_matrix(n):
     "Returns a Hilbert Matrix contructed by a given n"
-    H = [[0 for col in range(0,n+1)] for row in range(0,n)]
-    for row in range(0,n):
-        for col in range(0,n+1):
-            if (col != n):
-                H[row][col] = (1/float(col+row+1))
+    H = [[0 for __ in range(0,n+1)] for __ in range (0,n+1)]
+    for row in range(0,len(H)):
+        for col in range(0,len(H[row])):
+            if (col != len(H)-1):
+               H[row][col] = 1/float(col+row+1)
             else:
-                H[row][col] = quad(Hilbert_b, 0, 1, args=(row))[0]
+               H[row][col] = quad(Hilbert_b_integral, 0, 1, args=(row))[0]
+    #return [[ 1/float(col+row+1) for col in range(0,n)] for row in range(0,n)]
     return H
 
-   
+# Construct b solution of a Hilbert matrix 
+def Hilbert_matrix_b(n):
+    b = [[0 for __ in range(0,1)] for __ in range(0,n)]
+    for row in range(len(b)):
+        for col in range(len(b[row])):
+            b[row][col] = quad(Hilbert_b_integral, 0, 1, args=(row))[0]
+    return b
+    
+# Construct a Hilbert Matrix without b
+def Hilbert_matrix_without_b(n):
+    "Returns a Hilbert Matrix contructed by a given n"
+    H = [[0 for __ in range(0,n)] for __ in range (0,n)]
+    for row in range(0,n):
+        for col in range(0,n):
+            H[row][col] = 1/float(col+row+1)
+    return H
 
-A = np.array([[25,5,1,106.8],[64,8,1,177.2],[144,12,1,279.2]])
-b = [1,2,3]
-x = [1, 1, 1]
-B = [[25,5,1,106.8], [64,8,1,177.2],[144,12,1,279.2]]
-n = 20
-
-def solve_matrix (M, method):
+# Solves a matrix by a gauss method   
+def solve_matrix (Hb, method, b, H=None, x=None, n=100):
     if (method == 0):
-        print "\nNaive Gauss Elimination\n"
-        print backSub(rowReduce(B)) # naive
+        gauss_naive_sol = backSub(rowReduce(Hb),1)
+        r = residual_error(H,gauss_naive_sol,b)
+        rnorm = norm_residual_error(r)
+        print ("Method: Gauss-Naive\n")
+        printSolutions(Hb,H,gauss_naive_sol,b,r,rnorm) # naive
     elif (method == 1):
-        print "\nPartial Pivoting Gauss Elimination\n"
-        print backSub(partial_pivoting(B)) # partial
+        gauss_partial_sol = backSub(partial_pivoting(Hb),1)
+        r = residual_error(H,gauss_partial_sol,b)
+        rnorm = norm_residual_error(r)
+        print ("Method: Gauss-Partial-Pivoting\n")
+        printSolutions(Hb,H,gauss_partial_sol,b,r,rnorm) # partial
     elif (method == 2):
-        print "\nScaled Partial Pivoting Gauss Elimination\n"
-        print backSub(scaled_partial_pivoting(B)) # scaled partial
+        gauss_scaled_partial_pivoting = backSub(scaled_partial_pivoting(Hb),1)
+        r = residual_error(H,gauss_scaled_partial_pivoting,b)
+        rnorm = norm_residual_error(r)
+        print("Method: Gauss-Scaled-Partial-Pivoting\n")
+        printSolutions(Hb, H,gauss_scaled_partial_pivoting,b,r,rnorm) # scaled partial
     elif (method == 3):
         print "\nLU FACTORIZATION NOT IMPLEMENTED YET\n"
-        #print backSub(scaled_partial_pivoting(B)) # scaled partial
+        L = LU_factorization(H)[0] # gets L
+        U = LU_factorization(H)[1] # gets U
+        lu_sol = solveLU(L,U,b)
+        r = residual_error(H,lu_sol,b)
+        rnorm = norm_residual_error(r)
+        printSolutions(Hb, H,lu_sol,b,r,rnorm) # scaled partial
     else:
-        print "\nError aproximation in Gauss-Seider\n"
-        print Gauss_Seider(A, b, x, n)
-        
-        
+        gs = gauss_seidel(H, b)
+        r = residual_error(H,gs,b)
+        rnorm = norm_residual_error(r)
+        print("Method: Gauss-Seidel\n")
+        printSolutions(Hb, H,gs,b,r,rnorm) 
 
-"----------- TEST FUNCTIONS ------------"
+# calculates the residual error vector where r = Ax - b
+# and returns ||r||. That's it the norm of the r vector
+# which represents the residual vector of the solutions
+# A = matrix A
+# xap = matrix of aproximate solutions of x1,x2...
+# b = solution to the matrix
+def residual_error(A,x,b):
+    Ax = MultiplyMat(A,x) # Ax
+    r = []
+    for row in range(0,len(Ax)):
+        for col in range(len(Ax[row])):
+            r.append(b[row][col] - Ax[row][col])
+    return r
 
-solve_matrix(B, Method.NAIVE)
-solve_matrix(B, Method.PARTIAL)
-solve_matrix(B, Method.SCALED_PARTIAL)
-#solve_matrix(B, Method.GAUSS_SEIDER)
-print "\n Hilbert Matrix with n=3\n"
-print Hilbert_matrix(3) # construct a Hilber matrix of H(n)
+#Given a residual error vector calculates its norm   
+def norm_residual_error (r):
+    return math.sqrt(sum(abs(row) for row in r)) # return r     
+
+
+# Prints solutions in a nicely format
+def printSolutions(Hb,H,sol,b,r,rnorm):
+    x = MultiplyMat(H,sol)
+    print '{:<15} {:>35} {:>35} {:>53}'.format("Hilbert X solutions","Hilbert b solutions", "Compute Ax", "Residual Error Vector (r = b - Ax)")
+    for nrow in range(len(sol)):
+        for ncol in range(len(sol[nrow])):
+            tmpnrow = nrow + 1
+            print '{:>1} {:<30} {:<3} {:>12} {:>24} {:>12} {:>24} {:>1}'.format("x"+`tmpnrow`+" =", sol[nrow][ncol], 
+                  "b"+`tmpnrow`+" =",b[nrow][ncol], "Ax"+`tmpnrow`+" =",x[nrow],"r"+`tmpnrow`+" =",r[nrow])
+    print ("\nNorm of the residual error vector ||r|| = %e\n\n" %rnorm)
+
+# multipies matrix from a given vector   
+def matrixMul(A, B):
+    TB = zip(*B)
+    return [[sum(ea*eb for ea,eb in zip(a,b)) for b in TB] for a in A]
+
+# pivotizes for LU factorization
+def pivotize(m):
+    """Creates the pivoting matrix for m."""
+    n = len(m)
+    ID = [[float(i == j) for i in xrange(n)] for j in xrange(n)]
+    for j in xrange(n):
+        row = max(xrange(j, n), key=lambda i: abs(m[i][j]))
+        if j != row:
+            ID[j], ID[row] = ID[row], ID[j]
+    return ID
+
+# performs LU factorization and returns L,U and P
+def LU_factorization(A):
+    """Decomposes a nxn matrix A by PA=LU and returns L, U and P."""
+    n = len(A)
+    L = [[0.0] * n for i in xrange(n)]
+    U = [[0.0] * n for i in xrange(n)]
+    P = pivotize(A)
+    A2 = matrixMul(P, A)
+    for j in xrange(n):
+        L[j][j] = 1.0
+        for i in xrange(j+1):
+            s1 = sum(U[k][j] * L[i][k] for k in xrange(i))
+            U[i][j] = A2[i][j] - s1
+        for i in xrange(j, n):
+            s2 = sum(U[k][j] * L[i][k] for k in xrange(j))
+            L[i][j] = (A2[i][j] - s2) / U[j][j]
+    return (L, U, P)
+
+
+# Solves x's solutions from a matrix previosly descomposed by LU factorization        
+def solveLU(L,U,b):
+    A = MultiplyMat(L,U)
+    Ax = addColumn(A,b)
+    return backSub(rowReduce(Ax),1)
+       
+
+"----------- TEST FUNCTIONS ------------------------------------------------"
+
+for it in range(3,25):
+    print ("\n\n************ HILBERT MATRIX TEST ITERATION = %d *******************\n" %it)
+    Hb = Hilbert_matrix(it) # creates H with b
+    H = Hilbert_matrix_without_b(it) # creates H without b
+    b = Hilbert_matrix_b(it) # creates b
+    solve_matrix(Hb,Method.NAIVE, b,H) # solve H by Gauss-Naive
+    solve_matrix(Hb,Method.SCALED_PARTIAL, b,H) # solve H by Scaled Partial
+    solve_matrix(Hb,Method.LU_FACTORIZATION, b,H) # solve H by LU Factorization
+    solve_matrix(Hb,Method.GAUSS_SEIDEL, b,H) # Solve H by Gauss-Seidel 
+    
+    
+"------------------------------- END OF TESTING ----------------------------"
+  
+    
+    
+    
+    
+   
+
+
 
 
 
